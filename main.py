5046777dbbb91627030346842a4ec7331941697c
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-04-13 06:24:41 +0700

    add typing animation and reject bad questions

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import time
 openai.api_key = "your api key"
 
 question = None
-sec = 5
+sec = 10
+bot = "Bot: "
 def get_api_response(prompt: str) -> str | None:
     text: str | None = None
 
@@ -42,50 +43,21 @@ def create_prompt(message: str, pl: list[str]) -> str:
     prompt: str = ''.join(pl)
     return prompt
 
+def print_with_typing(text: str, delay: float):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-def get_bot_response(user_input: str, prompt_list: list[str]) -> str:
-    if 'i\'m naufal' in user_input.lower():
-        return "I know you are Naufal, I was created because of you."
-    elif 'i\'m dasha' in user_input.lower():
-        return "I Know You Are dasha, You are my owner's wife"
-    elif 'who' and 'naufal' in user_input.lower():
-        return "Naufal is my owner, he is very good at IT and he also interested at youtube"
-    elif 'thank' in user_input.lower():
-        return "Your Welcome, My Pleasure to help you..."
-    elif 'assalamualaikum' in user_input.lower():
-        time.sleep(2)
-        return "Wa'alaikumsalam, may i help you?"
-    elif 'go' and 'assalamualaikum' in user_input.lower():
-        return "waalaikumsalam, take care!"
-    elif 'shut' and 'down' in user_input.lower():
-        os.system(f'shutdown /s /t {sec}')
-        pyttsx3.speak(f'shutting down in {sec} seconds')
-        return ("Shutting down...")
-    else:
-        # Load questions and answers from JSON file
-        with open('my.json', 'r') as f:
-            my = json.load(f)
-        # Check if the user input matches a question in the JSON file
-        for question, answer in my.items():
-            if question.lower() in user_input.lower():
-                return answer
-        # If no match is found, generate a response using OpenAI
-        prompt = f'Human: {user_input}\nAI:'
-        bot_response = get_api_response(prompt)
-        if bot_response:
-            pos = bot_response.find(' ')
-            bot_response = bot_response[pos + 1:].strip()
-        else:
-            bot_response = "I'm sorry, I didn't understand your question."
-        time.sleep(2)
-        return bot_response
-
+                
+# main method with write animation
 def main():
-    prompt_list: list[str] = ['']
-
+    banner = "-"
+    print(f"{banner*7} MF Chatbot by abdFal {banner*7}")
     print("Can I help you?")
 
     while True:
@@ -93,10 +65,65 @@ def main():
         if user_input.lower() == 'bye':
             print('Bot: Bye Byee!')
             break
-        response: str = get_bot_response(user_input, prompt_list)
-        time.sleep(1)
-        print(f'Bot: {response}')
-
+        elif user_input.lower() == "i'm naufal":
+            print_with_typing(f'Bot: I know you are Naufal, I was created because of you.', 0.05)
+            continue
+        elif user_input.lower() == "i'm dasha":
+            print_with_typing(f'Bot: I Know You Are dasha, You are my owner\'s wife', 0.05)
+            continue
+        elif 'who' and 'god' in user_input.lower():
+            print_with_typing(f'Bot: There is no god, but Allah', 0.05)
+            continue
+        elif 'thank' in user_input.lower():
+            print_with_typing(f'Bot: Your Welcome, My Pleasure to help you...', 0.05)
+            continue
+        elif 'who' and "naufal" in user_input.lower():
+            print_with_typing(f'Bot: Naufal is my owner, he is very good at IT and he also interested at youtube.', 0.05)
+            continue
+        elif user_input.lower() == "assalamualaikum":
+            print_with_typing(f'Bot: Wa\'alaikumsalam, may i help you?', 0.05)
+            continue
+        elif user_input.lower() == "real":
+            print_with_typing(f'Bot: it\'s real, no cap', 0.05)
+            continue
+        elif "shut" and "down" in user_input.lower():
+            os.system(f'shutdown /s /t {sec}')
+            pyttsx3.speak(f'shutting down in {sec} seconds')
+            print_with_typing(f'Bot: Shutting down...', 0.05)
+            continue
+        else:
+            # Load questions and answers from JSON file
+            with open('my.json', 'r') as f:
+                my = json.load(f)
+            # Check if the user input matches a question in the JSON file
+            for question in my['questions']:
+                if isinstance(question['question'], list):
+                    for q in question['question']:
+                        if q.lower() in user_input.lower():
+                            answer = question['answer']
+                            print_with_typing(f'Bot: {answer}', 0.03)
+                            break
+                    else:
+                        continue
+                    break
+                else:
+                    if question['question'].lower() in user_input.lower():
+                        answer = question['answer']
+                        print_with_typing(f'Bot: {answer}', 0.03)
+                        break
+            else:
+                # If no match is found in JSON, generate a response using OpenAI
+                prompt = f'Human: {user_input}\nAI:'
+                bot_response = get_api_response(prompt)
+                if bot_response:
+                    pos = bot_response.find(' ')
+                    bot_response = bot_response[pos + 1:].strip()
+                else:
+                    bot_response = "I'm sorry, I didn't understand your question."
+                time.sleep(0.3)
+                print_with_typing(f"Bot: {bot_response}", 0.02)
+                
+        
 if __name__ == '__main__':
-    time.sleep(1)
+    time.sleep(0.5)
     main()
